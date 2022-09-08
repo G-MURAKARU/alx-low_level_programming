fac998094290f460ea9f20a232210db7b247f93e
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-09-08 14:44:45 -0700

    Correction on file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * function main, prints arbitrary string to the console
+ * main - prints arbitrary string to the console
+ *
+ * Return: 0 if code if successful, -1 for errors
  */
 int main(void)
 {
-  char my_string[] = "\"Programming is like building a multilingual puzzle";
-  puts(my_string);
-  return (0);
+char my_string[] = "\"Programming is like building a multilingual puzzle";
+puts(my_string);
+return (0);
 }

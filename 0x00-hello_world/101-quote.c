8ce583b8cd37be602cd9d0ca093e32716989d395
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-09-08 15:15:57 -0700

    Advanced tasks added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints an arbitrary string to the console using putchar
+ *
+ * Return: 1 if successful, -1 if errors
+ */
+int main(void)
+{
+char my_string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+int string_length = strlen(my_string);
+int counter = 0;
+while (counter < string_length)
+{
+putchar(my_string[counter]);
+counter++;
+}
+putchar('\n');
+return(1);
+}

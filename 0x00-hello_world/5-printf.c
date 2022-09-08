96b4be589c9fdf70a601e157912b99980d6aec54
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-09-08 14:51:10 -0700

    Commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
  * main - prints arbitrary string to the console
  *
  * Return: 0 if code is successful, -1 if errors
- */ 
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }

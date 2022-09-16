b608fce1855456340f3894c5c8eec26cd3eec9a6
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-09-16 04:46:11 -0700

    aye2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,30 +2,31 @@
 #include <stdlib.h>
 
 /**
- * fizz_buzz - Fizz Buzz
+ * main - Fizz Buzz
  *
  * Return: null
  */
 int main(void)
 {
 int counter;
-for (counter = 1; counter <= 100; counter++)
+printf("%d", 1);
+for (counter = 2; counter <= 100; counter++)
 {
 if (counter % 5 == 0 && counter % 3 == 0)
 {
-printf("FizzBuzz ");
+printf(" FizzBuzz");
 }
 else if (counter % 5 == 0)
 {
-printf("Buzz ");
+printf(" Buzz");
 }
 else if (counter % 3 == 0)
 {
-printf("Fizz ");
+printf(" Fizz");
 }
 else
 {
-printf("%d ", counter);
+printf(" %d", counter);
 }
 }
 printf("\n");

6ceab676870e19970feefa3595029515bb7f4440
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-09-27 05:36:29 -0700

    commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,23 @@
  */
 char *cap_string(char *str)
 {
-  int checker = 0;
-  int counter;
-  char my_array[13] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
-  while (*(str + checker) != '\0')
-    {
-      for (counter = 0; counter < 13; counter++)
-       {
-	if (*(str + checker) == my_array[counter])
-	  {
-	   if (*(str + checker + 1) >= 97 && *(str + checker + 1) <= 122)
-	     {
-	      *(str + checker + 1) -= 32;
-	     }
-	   break;
-	  }
-       }
-     checker++;
-    }
-  return (str);
+int checker = 0;
+int counter;
+char my_array[13] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
+while (*(str + checker) != '\0')
+{
+for (counter = 0; counter < 13; counter++)
+{
+if (*(str + checker) == my_array[counter])
+{
+if (*(str + checker + 1) >= 97 && *(str + checker + 1) <= 122)
+{
+*(str + checker + 1) -= 32;
+}
+break;
+}
+}
+checker++;
+}
+return (str);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,21 @@
  */
 char *leet(char *str)
 {
-  int checker = 0;
-  int counter;
-  char char_array[5] = {'a', 'e', 'o', 't', 'l'};
-  char int_array[5] = {'4', '3', '0', '7', '1'};
-  while (*(str + checker) != '\0')
-    {
-      for (counter = 0; counter < 5; counter++)
-	{
-	  if ((*(str + checker) == char_array[counter]) || (*(str + checker) == char_array[counter] - 32))
-	    {
-	      *(str + checker) = int_array[counter];
-	      break;
-	    }
-	}
-      checker++;
-    }
-  return (str);
+int checker = 0;
+int counter;
+char char_array[5] = {'a', 'e', 'o', 't', 'l'};
+char int_array[5] = {'4', '3', '0', '7', '1'};
+while (*(str + checker) != '\0')
+{
+for (counter = 0; counter < 5; counter++)
+{
+if ((*(str + checker) == char_array[counter]) || (*(str + checker) == char_array[counter] - 32))
+{
+*(str + checker) = int_array[counter];
+break;
+}
+}
+checker++;
+}
+return (str);
 }

4b8e4a3e85ea4053c6d69e24e50873b5d65b0fd8
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-06 09:19:17 -0700

    commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,22 +10,22 @@
  */
 int *array_range(int min, int max)
 {
-  int *array_pointer;
-  unsigned int counter = 0;
-  if (min > max)
-    {
-      return (NULL);
-    }
-  array_pointer = malloc(sizeof(int) * ((max - min) + 1));
-  if (array_pointer == NULL)
-    {
-      return (NULL);
-    }
-  while (min <= max)
-    {
-      array_pointer[counter] = min;
-      counter++;
-      min++;
-    }
-  return (array_pointer);
+int *array_pointer;
+unsigned int counter = 0;
+if (min > max)
+{
+return (NULL);
+}
+array_pointer = malloc(sizeof(int) * ((max - min) + 1));
+if (array_pointer == NULL)
+{
+return (NULL);
+}
+while (min <= max)
+{
+array_pointer[counter] = min;
+counter++;
+min++;
+}
+return (array_pointer);
 }

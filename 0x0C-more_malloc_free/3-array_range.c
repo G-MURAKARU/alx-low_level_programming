af6d91d0d85c0682bb4472de8577d0b55f3dbd65
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-06 09:12:22 -0700

    uploaded task 3 code files

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: lower limit of closed interval
+ * @max: upper limit of closed interval
+ *
+ * Return: pointer to array
+ */
+int *array_range(int min, int max)
+{
+  int *array_pointer;
+  unsigned int counter = 0;
+  if (min > max)
+    {
+      return (NULL);
+    }
+  array_pointer = malloc(sizeof(int) * ((max - min) + 1));
+  if (array_pointer == NULL)
+    {
+      return (NULL);
+    }
+  while (min <= max)
+    {
+      array_pointer[counter] = min;
+      counter++;
+      min++;
+    }
+  return (array_pointer);
+}

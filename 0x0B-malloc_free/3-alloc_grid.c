f2c85ed0cbf69c2e56fbb26db300db2024dba3b8
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-06 02:50:50 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,24 +11,30 @@
 int **alloc_grid(int width, int height)
 {
 int **two_D_pointers;
-int outer_loop = 0;
-int inner_loop = 0;
+int outer_loop;
+int inner_loop;
+int malloc_clearer;
 two_D_pointers = (int**)malloc(sizeof(int*) * height);
-if (two_D_pointers == NULL)
+if (two_D_pointers == NULL || height == 0)
 {
+free(two_D_pointers);
 return (NULL);
 }
- for (outer_loop = 0; outer_loop < height; outer_loop++)
-   {
-     two_D_pointers[outer_loop] = (int*)malloc(sizeof(int) * width);
-     if (two_D_pointers[outer_loop] == NULL)
-       {
-	 return (NULL);
-       }
-     for (inner_loop = 0; inner_loop < width; inner_loop++)
-       {
-	 two_D_pointers[outer_loop][inner_loop] = 0;
-       }
-   }
+for (outer_loop = 0; outer_loop < height; outer_loop++)
+{ 
+two_D_pointers[outer_loop] = (int*)malloc(sizeof(int) * width);
+if (two_D_pointers[outer_loop] == NULL || width == 0)
+{
+for (malloc_clearer = 0; malloc_clearer < outer_loop; malloc_clearer++)
+{
+free(two_D_pointers[malloc_clearer]);
+}
+return (NULL);
+}
+for (inner_loop = 0; inner_loop < width; inner_loop++)
+{
+two_D_pointers[outer_loop][inner_loop] = 0;
+}
+}
 return (two_D_pointers);
 }

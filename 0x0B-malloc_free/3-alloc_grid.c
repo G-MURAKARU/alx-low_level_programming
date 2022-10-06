0af0aa5a29d7e6a55e19bb3f8065ac77c4e228a7
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-06 02:05:28 -0700

    fixed file 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,19 +10,19 @@
  */
 int **alloc_grid(int width, int height)
 {
-  int *one_D_pointers;
-  int **two_D_pointers;
-  int outer_loop = 0;
-  int inner_loop = 0;
-  two_D_pointers = malloc(sizeof(**two_D_pointers) * height);
-  one_D_pointers = malloc(sizeof(int) * (width * height)); /* enough memory for one row */
-  for (outer_loop = 0; outer_loop < height; outer_loop++)
-    {
-      two_D_pointers[outer_loop] = (one_D_pointers + (outer_loop * width));
-      for (inner_loop = 0; inner_loop < width; inner_loop++)
-	{
-	  one_D_pointers[(outer_loop * width) + inner_loop] = 0;
-	}
-    }
-  return (two_D_pointers);
+int *one_D_pointers;
+int **two_D_pointers;
+int outer_loop = 0;
+int inner_loop = 0;
+two_D_pointers = malloc(sizeof(*one_D_pointers) * height);
+one_D_pointers = malloc(sizeof(int) * (width * height));
+for (outer_loop = 0; outer_loop < height; outer_loop++)
+{
+two_D_pointers[outer_loop] = one_D_pointers + (outer_loop * width);
+for (inner_loop = 0; inner_loop < width; inner_loop++)
+{
+one_D_pointers[(outer_loop * width) + inner_loop] = 0;
+}
+}
+return (two_D_pointers);
 }

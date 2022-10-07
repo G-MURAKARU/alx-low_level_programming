df9a897abb582b71af5ffee5164f0219487fbe0d
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-07 07:47:53 -0700

    uploaded task 7 code files

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,12 +13,12 @@ void print_diagsums(int *a, int size)
 int sum1 = 0;
 int sum2 = 0;
 int counter;
-for (counter = 0; counter < (size*size); counter += (size + 1))
+for (counter = 0; counter < (size * size); counter += (size + 1))
 {
 sum1 += a[counter];
 }
 printf("%d, ", sum1);
-for (counter = (size - 1); counter < ((size*size) - 1); counter += (size - 1))
+for (counter = (size - 1); counter < ((size * size) - 1); counter += (size - 1))
 {
 sum2 += a[counter];
 }

0374f5f5614683d5b605fa3b77fc2eb5ca34f565
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-07 07:22:35 -0700

    uploaded task 6 files

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,15 +8,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-  unsigned int counter1;
-  unsigned int counter2;
-  for (counter1 = 0; counter1 < 8; counter1++)
-    {
-      for (counter2 = 0; counter2 < 8; counter2++)
-	{
-	  _putchar((a[counter1][counter2]));
-	}
-      _putchar('\n');
-    }
-  return;
+unsigned int counter1;
+unsigned int counter2;
+for (counter1 = 0; counter1 < 8; counter1++)
+{
+for (counter2 = 0; counter2 < 8; counter2++)
+{
+_putchar((a[counter1][counter2]));
+}
+_putchar('\n');
+}
+return;
 }

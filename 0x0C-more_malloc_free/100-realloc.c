5b1a5dbb4756aca09a738c48c87f6e4171f69c8d
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-07 02:31:52 -0700

    uploaded task 100 files

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - reallocates a memory chunk elsewhere
+ * @ptr: pointer to first memory location
+ * @old_size: size of initial memory chunk
+ * @new_size: size of new memory chunk
+ *
+ * Return: pointer to new memory chunk
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+  void *array_pointer;
+  if (new_size == old_size)
+    {
+      return (ptr);
+    }
+  else if (new_size == 0 && ptr != NULL)
+    {
+      free(ptr);
+      return (NULL);
+    }
+  else
+    {
+      array_pointer = malloc(new_size);
+      if (array_pointer == NULL)
+	{
+	  return (NULL);
+	}
+      free(ptr);
+      return (array_pointer);
+    }
+}

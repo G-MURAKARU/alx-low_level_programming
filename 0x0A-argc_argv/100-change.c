21ad754ae01fdeeeabcc9e7e36baef83cfeadb53
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-08 05:22:45 -0700

    fixed file 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -52,6 +52,11 @@ if (argc != 2)
 printf("Error\n");
 return (1);
 }
+if ((atoi(argv[1])) == 0 && (argv[1] != 0))
+{
+printf("Error\n");
+return (1);
+}
 coins = change_maker(atoi(argv[1]), coins);
 printf("%d\n", coins);
 return (0);

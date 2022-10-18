20b48a861c6b0d8bab562ab3b38574984d39d8a5
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-18 05:26:42 -0700

    uploaded file 1 of variadic functions project

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: string that separates numbers when printed
+ * @n: number of input integers
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list input_integers;
+	unsigned int counter;
+
+	va_start(input_integers, n);
+	for (counter = 0; counter < (n - 1); counter++)
+	{
+		printf("%d", va_arg(input_integers, int));
+		if (separator != NULL)
+			printf("%s", separator);
+	}
+	printf("%d", va_arg(input_integers, int));
+	putchar('\n');
+	va_end(input_integers);
+}

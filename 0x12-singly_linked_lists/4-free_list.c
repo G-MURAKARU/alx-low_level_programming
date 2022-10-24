b3f6494857f55c1dd6ecbb29ec9bbb54399efeff
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-10-24 05:49:46 -0700

    uploaded task 4 code files

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: pointer to head node
+ *
+ * Return: nothing.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+	if (!head)
+		return;
+	while (1)
+	{
+		free(head->str);
+		temp = head->next;
+		free(head);
+		head = temp;
+		if (!head)
+			break;
+	}
+	return;
+}
+

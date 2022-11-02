f9aa1d38892919d502d667616bcf7455739ca3ac
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-11-02 13:31:21 -0700

    uploaded code file for task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fildes = open(filename, O_CREAT | O_WRONLY, 600);
+	fildes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fildes == -1)
 		return (-1);
